9394a04d18696ae7cd55b6f26838af15e563f512
Oksana <ksusha_bor@ukr.net>
2026-06-09 23:26:30 +0100

    Adding Api tests from lecture

---
+++ tests/api/sendnetwork.api.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('should send signin API request after UI login', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  await page.locator('#signinEmail').fill(process.env.TEST_EMAIL!);
+  await page.locator('#signinPassword').fill(process.env.TEST_PASSWORD!);
+
+  //promise, waits for login
+  const loginResponsePromise = page.waitForResponse(response =>
+    response.url().includes('/api/auth/signin') &&
+    response.request().method() === 'POST'
+  );
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  const loginResponse = await loginResponsePromise;
+
+  expect(loginResponse.status()).toBe(200);
+
+  const body = await loginResponse.json();
+
+  expect(body.status).toBe('ok');
+
+  await expect(page).toHaveURL(/panel\/garage/); 
+});
